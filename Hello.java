6c985d0dd14f371cc874090eca0317e275715183
waqasidrees <50921273+waqasidrees@users.noreply.github.com>
2021-06-11 22:20:55 +0500

    Update Hello.java
    added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class HEllo{
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
 }
