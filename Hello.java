3ec0b3b99c2804c0851938d54d255804b4268fcf
waqasidrees <50921273+waqasidrees@users.noreply.github.com>
2021-06-11 22:15:57 +0500

    commit
    added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-Public class HEllo {
-  Public static void public static void main(String[] args) {
+Public class HEllo{
+  Public static void main(String[] args) {
     System.out.println("Hello, World");
   }
 }
